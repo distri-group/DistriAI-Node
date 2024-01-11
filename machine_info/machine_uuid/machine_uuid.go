880635233d04d3b13ff42f16b7f8aa78a647d557
thirteenggh <thirteenggh@gmail.com>
2024-01-11 10:20:07 +0800

    test: Add debug

--- machine_info/machine_uuid/machine_uuid.go
+++ machine_info/machine_uuid/machine_uuid.go
@@ -2,8 +2,6 @@ package machine_uuid
 
 import (
 	"DistriAI-Node/utils/log_utils"
-	"os"
-	"strings"
 )
 
 type MachineUUID string
@@ -12,10 +10,10 @@ func GetInfoMachineUUID() (MachineUUID, error) {
 	logs.Normal("Getting machine ID...")
 
 	// Easy debugging
-	mID, err := os.ReadFile("/etc/machine-id")
-	if err != nil {
-		return "", err
-	}
-	return MachineUUID(strings.TrimSpace(string(mID))), nil
-	// return MachineUUID("b711ebf34e474f4db43198e23a59d416"), nil
+	// mID, err := os.ReadFile("/etc/machine-id")
+	// if err != nil {
+	// 	return "", err
+	// }
+	// return MachineUUID(strings.TrimSpace(string(mID))), nil
+	return MachineUUID("b711ebf34e474f4db43198e23a59d414"), nil
 }
