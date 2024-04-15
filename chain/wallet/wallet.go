0bdd579b2b565f60ed452a1a57ff8f4d95a14995
zyh_sir <zyh888686@gmail.com>
2024-04-15 09:53:30 +0800

    add annotation

--- chain/wallet/wallet.go
+++ chain/wallet/wallet.go
@@ -11,6 +11,8 @@ type Wallet struct {
 	Wallet *solana.Wallet
 }
 
+// InitWallet initialize solana wallet1
+// returns *Wallet and error when the public and private keys failed to be decoded
 func InitWallet(cfg *config.SolanaConfig) (*Wallet, error) {
 	payerPrivateKey := solana.MustPrivateKeyFromBase58(cfg.Key)
 	payerAccount, err := solana.WalletFromPrivateKeyBase58(payerPrivateKey.String())
