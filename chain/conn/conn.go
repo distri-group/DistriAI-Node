85e0a1827266d5e3961ed5a4361eea40993a9d9f
LYK <763158305@qq.com>
2024-06-28 11:49:56 +0800

    update

--- chain/conn/conn.go
+++ chain/conn/conn.go
@@ -29,24 +29,31 @@ func NewConn(cfg *config.SolanaConfig) (*Conn, error) {
 	return conn, nil
 }
 
+// SendAndConfirmTransaction sends a transaction via the RPC client and waits for its confirmation.
 func (conn *Conn) SendAndConfirmTransaction(tx *solana.Transaction) (string, error) {
+	 // Sends the transaction and acquires its signature
 	sig, err := conn.RpcClient.SendTransactionWithOpts(context.TODO(), tx, rpc.TransactionOpts{
 		SkipPreflight:       false,
 		PreflightCommitment: rpc.CommitmentFinalized})
 	if err != nil {
+		 // Returns an error if the transaction fails to send
 		return "", fmt.Errorf("> SendTransactionWithOpts, txSig: %s, err: %v", sig.String(), err)
 	}
 
+	// Waits for the transaction to be confirmed by the network
 	_, err = conn.WaitForConfirmation(sig.String())
 	if err != nil {
+		// Returns an error if confirmation is not successful
 		return "", fmt.Errorf("> WaitForConfirmation: %v", err)
 	}
 
+	// Returns the transaction signature upon successful confirmation
 	return sig.String(), nil
 }
 
 const TryLimit = 10
 
+// WaitForConfirmation waits for confirmation of a Solana transaction.
 func (conn *Conn) WaitForConfirmation(tx string) (*solana.Transaction, error) {
 	var transaction *solana.Transaction
 	txSig := solana.MustSignatureFromBase58(tx)
@@ -54,32 +61,40 @@ func (conn *Conn) WaitForConfirmation(tx string) (*solana.Transaction, error) {
 	var tryNum = 0
 	for {
 		tryNum++
+		// If the number of attempts exceeds the limit, return an error.
 		if tryNum > TryLimit {
 			return nil, fmt.Errorf("> TryLimit reached, tx: %s", tx)
 		}
+		// Sleep for 3 seconds between each attempt.
 		time.Sleep(3 * time.Second)
 
+		// Call the RPC client to get transaction details.
 		out, err := conn.RpcClient.GetTransaction(
 			context.TODO(),
 			txSig,
 			nil,
 		)
+		// If the transaction is not found, continue trying.
 		if err != nil {
 			if strings.Contains(err.Error(), "not found") {
 				continue
 			}
+			// Return other errors.
 			return nil, fmt.Errorf("> RpcClient.GetTransaction, tx: %s, err: %v", tx, err)
 		}
 
+		// Handle the case where 'out' is unexpectedly nil.
 		if out == nil {
 			logs.Warning("out is nil")
 			continue
 		}
 
+		// If there's an error in the transaction metadata, return it.
 		if out.Meta.Err != nil {
 			return nil, fmt.Errorf("> out.Meta.Err: %v", out.Meta.Err)
 		}
 
+		// Retrieve the transaction details; if successful, break the loop.
 		transaction, err = out.Transaction.GetTransaction()
 		if err != nil {
 			return nil, fmt.Errorf("> Transaction.GetTransaction, tx: %s, err: %v", tx, err)
