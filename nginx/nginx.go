c956cb4d28544dd6fabe396116ee98059c23cccb
ykking-0516 <156627183+ykking-0516@users.noreply.github.com>
2024-07-11 17:16:38 +0800

    Update nginx.go
    Added some comments

--- nginx/nginx.go
+++ nginx/nginx.go
@@ -9,11 +9,13 @@ import (
 
 func StartNginx(nginxPort, workPort, serverPort string) error {
 
+	// Generate Nginx configuration file
 	err := nginx_utils.GenNginxConfig(nginxPort, workPort, serverPort, pattern.ModleCreatePath)
 	if err != nil {
 		return fmt.Errorf("> gen nginx config: %v", err)
 	}
 
+	// Restart Nginx service
 	cmd := exec.Command("sudo", "service", "nginx", "restart")
 	err = cmd.Run()
 	if err != nil {
